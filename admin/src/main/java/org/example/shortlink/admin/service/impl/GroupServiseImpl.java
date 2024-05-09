6810734cd35d352f45ff939077271a40a60e2b9f
LLY <719194963@qq.com>
2024-05-09 19:59:16 +0800

    查询短链接分组-待完善

--- admin/src/main/java/org/example/shortlink/admin/service/impl/GroupServiseImpl.java
+++ admin/src/main/java/org/example/shortlink/admin/service/impl/GroupServiseImpl.java
@@ -1,5 +1,6 @@
 package org.example.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -7,9 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.shortlink.admin.dao.entity.GroupDO;
 import org.example.shortlink.admin.dao.mapper.GroupMapper;
 import org.example.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
+import org.example.shortlink.admin.dto.resq.ShortLinkGroupResqDTO;
 import org.example.shortlink.admin.service.GroupServise;
 import org.example.shortlink.admin.toolkit.RandomStringUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author LLY
@@ -29,10 +33,31 @@ public class GroupServiseImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
                 break;
             }
         }while (true);
-        GroupDO build = GroupDO.builder().gid(gid).name(saveReqDTO.getName()).build();
+        GroupDO build = GroupDO.builder()
+                .gid(gid)
+                .sortOrder(0)
+                .name(saveReqDTO.getName())
+                .build();
         baseMapper.insert(build);
 
     }
+
+    /**
+     * 查询分组
+     *
+     * @return
+     */
+    @Override
+    public List<ShortLinkGroupResqDTO> listGroup() {
+        //TODO 获取用户名
+        LambdaQueryWrapper<GroupDO> eq = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getDelFlag, 0)
+                .eq(GroupDO::getUsername, 1)
+                .orderByAsc(GroupDO::getSortOrder,GroupDO::getUpdateTime);
+        List<GroupDO> groupDOList = baseMapper.selectList(eq);
+        return BeanUtil.copyToList(groupDOList, ShortLinkGroupResqDTO.class);
+    }
+
     private boolean hasGroup(String gid){
         LambdaQueryWrapper<GroupDO> eq = Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getGid, gid)
