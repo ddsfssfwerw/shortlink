e2e7cfdcc333b77880f4257fde188ef2ae42b886
LLY <719194963@qq.com>
2024-05-11 09:57:48 +0800

    新增创建时间

--- project/src/main/java/org/example/shortlink/project/dto/resq/ShortLinkPageResqDTO.java
+++ project/src/main/java/org/example/shortlink/project/dto/resq/ShortLinkPageResqDTO.java
@@ -2,6 +2,7 @@ package org.example.shortlink.project.dto.resq;
 
 import cn.hutool.db.Page;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -63,7 +64,14 @@ public class ShortLinkPageResqDTO extends Page {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date validDate;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date createTime;
 
     /**
      * 描述
